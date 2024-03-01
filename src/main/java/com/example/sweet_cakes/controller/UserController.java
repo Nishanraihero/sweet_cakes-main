fc07ad8a5b2e1b2cf986883ab0083ea4ccd4a4fb
Nishanraihero <nishanrai2211@gmail.com>
2024-03-01 23:48:15 +0545

    first commit

---
+++ src/main/java/com/example/sweet_cakes/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.sweet_cakes.controller;
+
+import com.example.sweet_cakes.dto.UserDto;
+import com.example.sweet_cakes.entity.User;
+import com.example.sweet_cakes.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/data")
+    public String getData() {
+        return "data retrieved";
+    }
+
+    @PostMapping("/save")
+    public String createData(@RequestBody UserDto userDto) {
+        try {
+            userService.save(userDto);
+            return "created data";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error creating data: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAllData() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Optional<User> getById(@PathVariable("id") Integer id) {
+        return userService.getById(id);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id") Integer id) {
+        userService.deleteById(id);
+    }
+}
